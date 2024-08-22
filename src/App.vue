5da32e4d519ef41da044b2a5b2bef94aa7e1ac19
Dev_Snake <dangvanhaufpt2019@gmail.com>
2024-08-22 23:02:34 +0700

    init commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  </header>
+
+  <main>
+    <h1>Hello World</h1>
+  </main>
+</template>
+
+<style scoped></style>
